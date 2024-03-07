f823f050cbc460e9e485b39e1ab51a490b857c44
David Gilson <cs@david-gilson.com>
2024-03-07 15:33:28 +0100

    first commit

---
+++ Items/PropellerHat.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PropellerMod.Items
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class PropellerHat : ModItem
+    {
+		public override void SetDefault()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Purple + 11;
+			Item.vanity = true;
+		}
+
+		public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Feather, 5);
+            recipe.AddIngredient(ItemID.Silk, 5);
+            recipe.AddIngredient(3859, 1);
+			recipe.AddTile(TileID.Anvils);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+    }
+}
